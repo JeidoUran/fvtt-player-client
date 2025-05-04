749803f9a14019a7c3dd471b7c8cf7e509330cbc
Jeido <tourais.q@gmail.com>
2025-05-04 13:25:47 +0200

    remove default presence, only initialise it when logged in

--- src/richPresenceControl.ts
+++ src/richPresenceControl.ts
@@ -9,36 +9,21 @@ let rpcStartTime: Date | null = null;
 
 export async function enableRichPresence(windowId: number) {
   if (enabledWindows.has(windowId)) return;
-
   enabledWindows.add(windowId);
-  console.log(`[RPC] Activation demandée pour la fenêtre #${windowId}`);
 
-  if (!rpc) {
-    rpc = new Client({
-      clientId: '1366841898693562540',
-    });
-  }
+  if (!rpc) rpc = new Client({ clientId: '1366841898693562540' });
 
   if (!rpcInitialized) {
     try {
       await rpc.login();
       console.log('✅ Discord RPC connecté.');
-  
-      rpc.user?.setActivity({
-        details: 'Browsing Game Worlds',
-        state: 'In Setup Page',
-        largeImageKey: 'logo_fvtt_rp',
-        largeImageText: 'Foundry VTT Client',
-        startTimestamp: new Date(),
-        instance: false
-      });
       rpcInitialized = true;
-      console.log(`[RPC] Rich Presence connecté`);
     } catch (err) {
-      console.warn(`[RPC] Échec de connexion Discord :`, err);
+      console.warn('[RPC] Échec de connexion Discord :', err);
     }
   }
 }
+
 
 export async function updateActivity(data: {
   actorName: string;
@@ -54,13 +39,18 @@ export async function updateActivity(data: {
   worldId: string;
   sceneId: string;
 }) {
-  if (!rpcInitialized || !rpc || !rpc.user) return;
+  if (!rpcInitialized) {
+    await enableRichPresence(0);
+  }
+  if (!rpcInitialized || !rpc?.user) return;
+
 
   if (!rpcStartTime) rpcStartTime = new Date();
 
   rpc.user.setActivity({
     details: data.isGM
-      ? "Game Master" : `${data.actorName ? `${data.actorName}` : "Spectator"} ${data.hp ? ` - HP ${data.hp.value}/${data.hp.max}` : ""}`,
+      ? "Game Master"
+      : `${data.actorName ? `${data.actorName}` : "Spectator"} ${data.hp ? ` - HP ${data.hp.value}/${data.hp.max}` : ""}`,
       state: data.inCombat
         ? `In Battle`
         : `Exploring`,    
