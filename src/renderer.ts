1ddfc19dae3bc69e70c598917a5963a7f5148221
Jeido <tourais.q@gmail.com>
2025-05-10 21:11:42 +0200

    add hiding / displaying server info elements individually

--- src/renderer.ts
+++ src/renderer.ts
@@ -236,6 +236,7 @@ document
 
     await window.api.saveAppConfig(validConfig);
     applyAppConfig(validConfig);
+    refreshAllServerInfos();
     showNotification("Changes saved");
   });
 
@@ -1768,11 +1769,33 @@ async function getServerInfo(
 }
 
 async function updateServerInfos(gameItem: HTMLElement, game: GameConfig) {
-  const serverInfo = await getServerInfo(game);
+  // Retrieve user config
+  const { serverInfoEnabled = true, serverInfoOptions } =
+    await window.api.localAppConfig();
 
-  const serverInfos = gameItem.querySelector(".server-infos");
+  const serverInfos = gameItem.querySelector(
+    ".server-infos",
+  ) as HTMLDivElement | null;
   if (!serverInfos) return;
 
+  // If global toggle is off, hide everything and return
+  if (!serverInfoEnabled) {
+    serverInfos.style.display = "none";
+    return;
+  }
+  serverInfos.style.display = "";
+
+  // Individual options and their defaults
+  const {
+    statusEnabled = true,
+    foundryVersionEnabled = true,
+    worldEnabled = false,
+    gameSystemEnabled = true,
+    gameSystemVersionEnabled = true,
+    onlinePlayersEnabled = true,
+  } = serverInfoOptions;
+
+  // Retrieve each <span> and apply show/hide
   const statusSpan = serverInfos.querySelector(".status") as HTMLSpanElement;
   const versionSpan = serverInfos.querySelector(".version") as HTMLSpanElement;
   const worldSpan = serverInfos.querySelector(".world") as HTMLSpanElement;
@@ -1782,27 +1805,73 @@ async function updateServerInfos(gameItem: HTMLElement, game: GameConfig) {
   ) as HTMLSpanElement;
   const usersSpan = serverInfos.querySelector(".users") as HTMLSpanElement;
 
-  if (!serverInfo) {
-    statusSpan.innerHTML = `<i class="fa-solid fa-xmark"></i> Offline`;
-    versionSpan.innerHTML = `<i class="fa-solid fa-dice-d20"></i> -`;
-    worldSpan.innerHTML = `<i class="fa-solid fa-globe"></i> -`;
-    systemSpan.innerHTML = `<i class="fa-solid fa-dice"></i> -`;
-    systemVersionSpan.innerHTML = `<i class="fa-solid fa-screwdriver-wrench"></i> -`;
-    usersSpan.innerHTML = `<i class="fa-solid fa-users"></i> -`;
+  const statusWrapper = statusSpan.closest(".tooltip-wrapper") as HTMLElement;
+  const versionWrapper = versionSpan.closest(".tooltip-wrapper") as HTMLElement;
+  const worldWrapper = worldSpan.closest(".tooltip-wrapper") as HTMLElement;
+  const systemWrapper = systemSpan.closest(".tooltip-wrapper") as HTMLElement;
+  const systemVersionWrapper = systemVersionSpan.closest(
+    ".tooltip-wrapper",
+  ) as HTMLElement;
+  const usersWrapper = usersSpan.closest(".tooltip-wrapper") as HTMLElement;
+
+  if (statusWrapper) statusWrapper.style.display = statusEnabled ? "" : "none";
+  if (versionWrapper)
+    versionWrapper.style.display = foundryVersionEnabled ? "" : "none";
+  if (worldWrapper) worldWrapper.style.display = worldEnabled ? "" : "none";
+  if (systemWrapper)
+    systemWrapper.style.display = gameSystemEnabled ? "" : "none";
+  if (systemVersionWrapper)
+    systemVersionWrapper.style.display = gameSystemVersionEnabled ? "" : "none";
+  if (usersWrapper)
+    usersWrapper.style.display = onlinePlayersEnabled ? "" : "none";
+
+  // Ping server
+  const info = await getServerInfo(game);
+
+  // If it fails, displays "-"
+  if (!info) {
+    if (statusEnabled) {
+      statusSpan.innerHTML = `<i class="fa-solid fa-xmark"></i> Offline`;
+    }
+    if (foundryVersionEnabled) {
+      versionSpan.innerHTML = `<i class="fa-solid fa-dice-d20"></i> -`;
+    }
+    if (worldEnabled) {
+      worldSpan.innerHTML = `<i class="fa-solid fa-globe"></i> -`;
+    }
+    if (gameSystemEnabled) {
+      systemSpan.innerHTML = `<i class="fa-solid fa-dice"></i> -`;
+    }
+    if (gameSystemVersionEnabled) {
+      systemVersionSpan.innerHTML = `<i class="fa-solid fa-screwdriver-wrench"></i> -`;
+    }
+    if (onlinePlayersEnabled) {
+      usersSpan.innerHTML = `<i class="fa-solid fa-users"></i> -`;
+    }
     return;
   }
 
-  if (serverInfo.version) {
-    statusSpan.innerHTML = `<i class="fa-solid fa-signal"></i> Online`;
-  } else {
-    statusSpan.innerHTML = `<i class="fa-solid fa-xmark"></i> Offline`;
+  // Otherwise, inject real data
+  if (statusEnabled) {
+    statusSpan.innerHTML = info.version
+      ? `<i class="fa-solid fa-signal"></i> Online`
+      : `<i class="fa-solid fa-xmark"></i> Offline`;
   }
-
-  versionSpan.innerHTML = `<i class="fa-solid fa-dice-d20"></i> v${serverInfo.version ?? "-"}`;
-  worldSpan.innerHTML = `<i class="fa-solid fa-globe"></i> ${serverInfo.world ?? "-"}`;
-  systemSpan.innerHTML = `<i class="fa-solid fa-dice"></i> ${serverInfo.system?.toUpperCase() ?? "-"}`;
-  systemVersionSpan.innerHTML = `<i class="fa-solid fa-screwdriver-wrench"></i> ${serverInfo.systemVersion ?? "-"}`;
-  usersSpan.innerHTML = `<i class="fa-solid fa-users"></i> ${serverInfo.users ?? "0"}`;
+  if (foundryVersionEnabled) {
+    versionSpan.innerHTML = `<i class="fa-solid fa-dice-d20"></i> v${info.version ?? "-"}`;
+  }
+  if (worldEnabled) {
+    worldSpan.innerHTML = `<i class="fa-solid fa-globe"></i> ${info.world ?? "-"}`;
+  }
+  if (gameSystemEnabled) {
+    systemSpan.innerHTML = `<i class="fa-solid fa-dice"></i> ${info.system?.toUpperCase() ?? "-"}`;
+  }
+  if (gameSystemVersionEnabled) {
+    systemVersionSpan.innerHTML = `<i class="fa-solid fa-screwdriver-wrench"></i> ${info.systemVersion ?? "-"}`;
+  }
+  if (onlinePlayersEnabled) {
+    usersSpan.innerHTML = `<i class="fa-solid fa-users"></i> ${info.users ?? "0"}`;
+  }
 }
 
 async function refreshAllServerInfos() {
