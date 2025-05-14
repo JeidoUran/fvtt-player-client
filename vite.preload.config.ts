10656a009d4be9049f636e6bf61626088dd57276
Jeido <tourais.q@gmail.com>
2025-05-14 19:24:28 +0200

    push the configs that i forgot to push like a turbo-moron

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,23 +1,37 @@
 // noinspection JSUnusedGlobalSymbols
-
+// vite.preload.config.ts
 import { defineConfig } from "vite";
-import path from "path";
+import { resolve } from "path";
+import { builtinModules } from "module";
+import pkg from "./package.json";
 
 export default defineConfig({
   build: {
-    outDir: path.resolve(__dirname, ".vite/build"),
+    // ─── BUILD AS A CJS LIBRARY FOR PRELOAD ───────────────────
+    lib: {
+      entry: resolve(__dirname, "src/preload.ts"),
+      formats: ["cjs"],
+      fileName: () => "preload.js", // → .vite/build/preload.js
+    },
+    emptyOutDir: false,
+    outDir: resolve(__dirname, ".vite/build"),
     target: "es2022",
     minify: "esbuild",
-    sourcemap: false,
-    emptyOutDir: false,
     rollupOptions: {
-      external: ["ws"],
+      external: [
+        ...builtinModules,
+        ...Object.keys(pkg.dependencies || {}),
+        "electron",
+      ],
       output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        exports: "named",
         compact: true,
       },
     },
-  },
-  esbuild: {
-    drop: ["console", "debugger"],
   },
 });
